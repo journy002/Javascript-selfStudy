2825fca0205abeb97bce0afd782f61043460f165
journy002 <journy002@naver.com>
2021-12-12 21:39:52 +0900

    push, pop, shift, unshift 공부

--- 배열/test.js
+++ 배열/test.js
@@ -67,3 +67,62 @@ object.push({
 
 console.log(object)
 console.log(object.length, 'length')
+
+// pop, push, shift, unshift
+
+// push - 맨 끝에 요소를 추가
+// shift - 제일 앞 요소를 꺼내 제거한 후 남아있는 요소들을 앞으로 밀어줍니다. 제거한 요소를 반환합니다.
+// pop - 제일 끝 요소를 추출합니다.
+// upshift - 배열 앞에 요소를 추가합니다.
+
+// 큐(queue)는 배열을 사용해 만들 수 있는 대표적인 자료구조로, 배열과 마찬가지로 순서가 있는 컬렉션을 저장하는 데 사용합니다.
+
+// 큐 주요 연산
+// push
+// shift
+
+// 배열엔 두 연산을 가능하게 해주는 내장 메서드 push와 pop이 있습니다.
+// 화면에 순차적으로 띄울 메세지를 비축해 놓을 자료 구조를 만들 때 큐를 사용하는 것처럼 큐는 실무에서 상당히 자주 쓰이는 자료구조입니다.
+
+// 배열은 큐 이외에 스택(stack)이라 불리는 자료구조를 구현할 때도 쓰입니다.
+
+// 스택 주요 연산
+// push
+// pop
+
+// 스택은 이처럼 '한쪽 끝'에 요소를 더하거나 뺄 수 있게 해주는 자료구조입니다.
+// 스택은 흔히 카드 한 벌과 비교됩니다. 쌓여있는 카드 맨 위에 새로운 카드를 더해주거나 빼는 것처럼 스택도 '한쪽 끝'에 요소를 집어넣거나 추출 할 수 있기 때문입니다.
+
+// 스택을 사용하면 가장 나중에 집어넣은 요소가 먼저 나옵니다. 이 특징을 줄여 후입선출 (Last-In-First-Out)
+// 큐를 사용하면 먼저 집어넣은 요소가 먼저 나오기 때문에 선입선출(First-In-First-Out) 자료구조라고 부릅니다.
+
+// 처음이나 끝에 요소를 더하거나 빼주는 연산을 제공하는 자료구조 컴퓨터 과학 분야에선 데큐(deque, Double Ended Queue)라고 부릅니다.
+
+// pop
+// 배열 끝 요소를 제거하고, 제거한 요소를 반환합니다.
+
+let langu = ['a', 'b', 'c', 'd']
+let emptySpa = []
+emptySpa = langu.pop() // ['a','b','c']
+console.log(langu, 'pop')
+console.log(emptySpa, 'empty space')
+
+// push
+// 배열 끝에 요소를 추가합니다.
+
+langu.push('oo')
+console.log(langu, 'push')
+
+// shift
+// 배열 앞 요소를 제거하고, 제거한 요소를 반환합니다.
+
+// unshift
+// 배열 앞에 요소를 추가합니다.
+langu.unshift('unshift')
+console.log(langu, 'check unshift')
+
+// sort
+// 배열의 요소를 적절한 위치에 정렬한 후 그 배열을 반환합니다.
+let langu2 = ['c', 'b', 'a', 'x', 'd']
+langu2.sort()
+console.log(langu2, 'sort func')
