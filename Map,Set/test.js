b61dad6adb863e8127fb5b2f294f79e640aca747
journy002 <journy002@naver.com>
2021-12-15 14:01:42 +0900

    map set

---
+++ Map,Set/test.js
@@ -0,0 +1,14 @@
+// Map, Set
+
+// Map : 맵은 키가 있는 데이터를 저장한다는 점에서 객체와 유사합니다.
+// 맵은 키에 다양한 자료형을 허용한다는 점에서 차이가 있습니다.
+
+// 주요 메소드, 프로퍼티
+
+// new Map() - 맵을 만듭니다.
+// map.set(key, value) - key를 이용해 value를 저장합니다.
+// map.get(key) - key에 해당하는 값을 반환합니다. key가 존재하지 않으면 undefined를 반환합니다.
+// map.has(key) - key가 존재하면 treu, key가 존재하지 않으면 false를 반환합니다.
+// map.delete(key) - key에 해당하는 값을 삭제합니다.
+// map.clear() - 맵 안의 모든 요소를 삭제합니다.
+// map.size() - 요소의 개수를 반환합니다.
